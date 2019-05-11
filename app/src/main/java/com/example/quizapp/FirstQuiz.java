d235b248a73ad3a3317e4ff9b2b6ac580b5f4b7e
ismael <ismael.amadou@bluecrest.edu.gh>
2019-05-11 00:11:25 +0000

    quiz

---
+++ app/src/main/java/com/example/quizapp/FirstQuiz.java
@@ -0,0 +1,60 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+
+/**
+ * Created by Prosen on 4/5/2016.
+ */
+public class FirstQuiz extends AppCompatActivity {
+    private RadioGroup radioGroup;
+    private Button nxt;
+    int pos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz1);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+
+        radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId){
+
+
+                if (checkedId == R.id.radio1) {
+
+                    setContentView(R.layout.final2);
+
+                }
+                else if (checkedId == R.id.radio2) {
+
+                    setContentView(R.layout.final2);
+                }
+                else if (checkedId == R.id.radio3) {
+
+                    setContentView(R.layout.final1);
+
+                }
+                else{
+
+                    setContentView(R.layout.final2);
+
+
+                }
+
+
+
+            }
+        });
+
+    }
+
+}
